36ddec2124ea20383a86026596a44711b7be9ab0
simon <simon@dotdotbuy.com>
2016-08-04 11:27:10 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by simon on 2016/8/3.
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!5434534</h2>
+</body>
+</html>
